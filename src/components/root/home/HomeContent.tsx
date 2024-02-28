e3c9820e34718826d9fe4563d40be6d47d5561b4
mucahittasan <mucahittasan0@gmail.com>
2024-02-28 15:13:44 +0300

    created HomeContent page

--- src/components/root/home/HomeContent.tsx
+++ src/components/root/home/HomeContent.tsx
@@ -1,5 +1,24 @@
+// Utils
+import { MotionSection } from '@/utils/motions/Motions'
+import { containerVariants } from '@/utils/motions/Variant'
+
 const HomeContent = () => {
-  return <div>HomeContent</div>
+  return (
+    <MotionSection
+      className="content text-center h-[calc(100vh-80px)] flex px-4 flex-col justify-center"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <h2 className="lg:text-8xl md:text-7xl sm:text-5xl xs:text-4xl text-3xl font-bold">
+        Embark on a New Career Journey{' '}
+      </h2>
+      <p className="md:text-base text-sm mt-16 mx-auto max-w-[550px]">
+        Strengthen your career, explore the latest job opportunities, and shape
+        your future.
+      </p>
+    </MotionSection>
+  )
 }
 
 export default HomeContent
