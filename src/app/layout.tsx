93a01cc674fa98d41ef5dad7338516840f58d6cc
mucahittasan <mucahittasan0@gmail.com>
2024-02-28 15:03:57 +0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'Acme',
+  description:
+    'Explore a variety of job opportunities with Acme. Find the perfect job that suits your skills and interests. Discover a range of positions in different industries and start your career journey today.',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
