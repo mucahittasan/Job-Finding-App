7b6e961786bbe9bbc5ed0e31733fa4eabf5bf803
mucahittasan <mucahittasan0@gmail.com>
2024-02-29 10:22:05 +0300

    Initialize JobList constant with sample job data

---
+++ src/constants/JobList.ts
@@ -0,0 +1,111 @@
+export const jobs = [
+  {
+    name: 'Chief Research Supervisor',
+    createdAt: '2023-09-10T19:10:02.461Z',
+    location: 'Port Charlotte',
+    companyName: 'Gerhold - Gulgowski',
+    keywords: ['quos', 'ex', 'dicta', 'commodi', 'ex'],
+    id: '44e7a2c8-6517-4ac1-9d07-dc86ffb33666-1694373334337',
+    description:
+      'Officiis perferendis totam repellat nostrum in. Saepe laudantium quae numquam aspernatur quod eius impedit molestiae tempora.',
+    salary: 22918,
+  },
+  {
+    name: 'Principal Brand Orchestrator',
+    createdAt: '2023-09-10T19:06:51.023Z',
+    location: 'Irving',
+    companyName: 'Leannon and Sons',
+    keywords: ['non', 'delectus', 'minus', 'cumque', 'provident'],
+    id: 'dddaa4c2-157e-4e4b-b6dd-cdc2143f09a0-1694373334339',
+    description:
+      'Quisquam fugit inventore ut aperiam dolorem. Eius veritatis illo recusandae repellendus asperiores.',
+    salary: 22088,
+  },
+  {
+    name: 'Human Paradigm Manager',
+    createdAt: '2023-09-10T18:49:54.464Z',
+    location: 'Kettering',
+    companyName: 'Bosco and Sons',
+    keywords: ['est', 'mollitia', 'voluptas', 'vitae', 'adipisci'],
+    id: '0daea694-e755-43d9-9645-bcfc99e2acf2-1694373334341',
+    description: 'Velit nobis a delectus. Iste itaque iste sit eius fuga.',
+    salary: 20363,
+  },
+  {
+    name: 'Lead Intranet Facilitator',
+    createdAt: '2023-09-10T18:21:47.697Z',
+    location: 'East Daishashire',
+    companyName: 'Koch - Hilll',
+    keywords: ['non', 'voluptatibus', 'non', 'adipisci', 'dicta'],
+    id: 'b5491e35-65ea-4a89-baf8-415a7bd96d2f-1694373334331',
+    description:
+      'Repellendus eveniet eaque placeat fugiat ut labore. Totam repellendus ullam totam repellendus rem exercitationem.',
+    salary: 18742,
+  },
+  {
+    name: 'Global Paradigm Director',
+    createdAt: '2023-09-10T18:12:08.098Z',
+    location: 'West Charlenefort',
+    companyName: 'Hintz - Hayes',
+    keywords: ['mollitia', 'dicta', 'voluptas', 'delectus', 'adipisci'],
+    id: 'd43be716-3667-4ae1-905d-9d0ca9910c9f-1694373334338',
+    description:
+      'Suscipit quasi adipisci asperiores beatae harum. Ipsam nisi facilis quas laudantium dolorem.',
+    salary: 19937,
+  },
+  {
+    name: 'District Interactions Administrator',
+    createdAt: '2023-09-10T18:03:11.931Z',
+    location: 'Albertstad',
+    companyName: 'Hills, Feeney and Russel',
+    keywords: ['praesentium', 'delectus', 'molestiae', 'nesciunt', 'quos'],
+    id: 'efa27d5e-6dd5-4c9d-b03e-3f489e618956-1694373334330',
+    description:
+      'Minus natus sit repellat tenetur. Voluptatum enim accusamus quasi.',
+    salary: 22443,
+  },
+  {
+    name: 'Direct Communications Associate',
+    createdAt: '2023-09-10T18:01:46.106Z',
+    location: 'Townebury',
+    companyName: 'Osinski, Corkery and Kemmer',
+    keywords: ['harum', 'ex', 'etat', 'harum', 'etat'],
+    id: 'bb23f749-a4ce-4ea5-bce7-3569aa913598-1694373334339',
+    description:
+      'Alias earum laudantium nulla. Architecto eligendi esse pariatur.',
+    salary: 21915,
+  },
+  {
+    name: 'Direct Paradigm Producer',
+    createdAt: '2023-09-10T17:41:43.600Z',
+    location: 'Lake Melisaville',
+    companyName: 'Oberbrunner LLC',
+    keywords: ['quos', 'quos', 'dolores', 'quos', 'mollitia'],
+    id: '4a73536a-e0df-4868-9ddb-ebf47f44f1c8-1694373334337',
+    description:
+      'Voluptates libero minima. Saepe alias occaecati delectus atque iste reprehenderit.',
+    salary: 17464,
+  },
+  {
+    name: 'Human Intranet Strategist',
+    createdAt: '2023-09-10T17:12:23.089Z',
+    location: 'Emiefield',
+    companyName: 'Kshlerin - Barrows',
+    keywords: ['minima', 'provident', 'illo', 'adipisci', 'praesentium'],
+    id: 'e23eb6fe-8c23-4820-a3d8-ce687b2c811a-1694373334331',
+    description:
+      'Illo quod necessitatibus officiis necessitatibus necessitatibus consequatur pariatur tempora. Alias ullam esse eveniet excepturi repellendus nostrum necessitatibus molestias.',
+    salary: 23327,
+  },
+  {
+    name: 'Product Program Administrator',
+    createdAt: '2023-09-10T17:08:29.373Z',
+    location: "O'Connellberg",
+    companyName: 'Brekke, Wisozk and Glover',
+    keywords: ['hic', 'minus', 'est', 'ex', 'adipisci'],
+    id: '2ece34de-c87d-4c14-8bf2-18db5f526135-1694373334339',
+    description:
+      'Tempore velit libero incidunt nisi esse dicta tempora debitis maiores. Architecto nemo pariatur tempora accusantium asperiores blanditiis in.',
+    salary: 19197,
+  },
+]
